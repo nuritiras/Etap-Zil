d77ef327e625af4a86a9ce215d99fc07be0d3060
Nuri TIRAŞ <nuritiras@gmail.com>
2022-05-11 10:30:10 +0300

    Duyuru veritabani güncellendi.

--- okulzili/models.py
+++ okulzili/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class DuyuruModel(models.Model):
     title=models.CharField(max_length=50,verbose_name="Başlık Bilgisi",null=False,blank=False)
-    description=models.TextField(max_length=500,verbose_name="İçerik Bilgisi",null=False,blank=False)
+    description=models.TextField(max_length=5000,verbose_name="İçerik Bilgisi",null=False,blank=False)
