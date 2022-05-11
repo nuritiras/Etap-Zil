3549864da6294219b111ccfda9a73db13d280f2f
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2022-05-11 11:20:30 +0300

    Update settings.py

--- okul/settings.py
+++ okul/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'tr'
+LANGUAGE_CODE = 'tr-TR'
 
 TIME_ZONE = 'Europe/Istanbul'
 
