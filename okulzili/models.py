8cf7be225bd0fa8dab53c55453ee8cc001ee4541
Nuri TIRAŞ <nuritiras@gmail.com>
2022-05-12 02:20:13 +0300

    Veritabanı güncellendi.

--- okulzili/models.py
+++ okulzili/models.py
@@ -7,6 +7,7 @@ class DuyuruModel(models.Model):
     description=models.TextField(max_length=5000,verbose_name="İçerik Bilgisi",null=False,blank=False)
 
 class SaatModel(models.Model):
+    dersSaati=models.CharField(max_length=50,verbose_name="Ders Saati",null=False,blank=False,default="1. Ders")
     ogrenci=models.TimeField(verbose_name="Öğrenci Zili")
     ogretmen=models.TimeField(verbose_name="Öğretmen Zili")
     cikis=models.TimeField(verbose_name="Çıkış Zili")
